c8ee985d8cf405bb34d6937c31420917090b4431
Rose <120544840+Rose-my@users.noreply.github.com>
2024-06-07 12:38:04 +0900

    feat: 카테고리추출

---
+++ 카테고리/카테고리추출.py
@@ -0,0 +1,16 @@
+data=pd.read_csv('/content/drive/MyDrive/Colab Notebooks/졸업프로젝트/bunsilmul_0603.csv')
+data.head()
+
+!pip install webcolors
+
+'''
+import webcolors
+
+# 한국어 색상명을 영어로 변환
+print(webcolors.name_to_rgb('빨강')) # (255, 0, 0)
+print(webcolors.rgb_to_name((255, 0, 0))) # 'red'
+
+# 16진수 색상 코드와 변환
+print(webcolors.name_to_hex('파란색')) # '#0000ff'
+print(webcolors.hex_to_name('#0000ff')) # 'blue'
+  '''
